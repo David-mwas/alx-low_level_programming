c221c8c29b1ec0fabf80034e891e52416e58f0cd
David-mwas <dmwas704@gmail.com>
2022-09-10 02:21:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main-print puts func string
+ *
+ *Description:use func main
+ *this program prints "programming is like building a multilingual puzzle
+ *Return:0
+ */
+int main(void)
+{
+printf("with proper grammar , but the outcome is a piece of art,\n");
+
+return (0);
+}
