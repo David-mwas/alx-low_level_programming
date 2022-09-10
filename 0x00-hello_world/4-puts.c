c1d7cd712ae0fe4c9062f7990a7fb396cbb97aca
David-mwas <dmwas704@gmail.com>
2022-09-10 01:19:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,15 @@
-#include <stdio.h>/***main-print put string
-**Description:use func main
-*this program print "programming is like building a multilingual puzzle
-*Return:0*/int main(void){puts("\"Programming is like building a multilingual puzzle "); return (0);}
+#include <stdio.h>
+
+/**
+ *main-print puts func string
+ *
+ *Description:use func main
+ *this program prints "programming is like building a multilingual puzzle
+ *Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+
+return (0);
+}
