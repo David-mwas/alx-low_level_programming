bced564795663dd441a96f91081068842dfa8ad1
David-mwas <dmwas704@gmail.com>
2022-09-12 17:55:36 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-Entry point print alphabs except q,e
+ *
+ *Return:Always 0 (success)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = z ; ch >= a ; ch--)
+{
+putchar(ch);
+}
+
+putchar(n);
+return (0);
+}
