a601742396b3373577d62ffe5b74d09a07ff2c69
David-mwas <dmwas704@gmail.com>
2022-09-12 17:10:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main-Entry point print alphabs except q,e
+ *
+ *Return:Always 0 (success)
+ */
+int main(void)
+{
+char l;
+
+for (l = a ; l <= z ; l++)
+{
+putchar(l);
+}
+putchar(n);
+return (0);
+}
