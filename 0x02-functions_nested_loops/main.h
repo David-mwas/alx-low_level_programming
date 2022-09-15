ac4486e8c1118147b5ef64505b2b83e4cd11bcb2
David-mwas <dmwas704@gmail.com>
2022-09-15 12:12:58 -0700

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c); 
 void print_alphabet(void); 
 void print_alphabet_x10(void); 
@@ -9,7 +10,7 @@ int _abs(int);
 int print_last_digit(int); 
 void jack_bauer(void); 
 void times_table(void); 
-int add(int, int); 
+int add(int, int); 
 void print_to_98(int n); 
 void print_times_table(int n);
-
+#endif
