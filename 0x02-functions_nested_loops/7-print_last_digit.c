bd8399cd0ba875ac101329ef5d78339f038c3c46
David-mwas <dmwas704@gmail.com>
2022-09-15 15:19:11 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_last_digit(int n);
+ *"print_last_digit":checks for lowerc characters
+ *"n"-is the number printed last
+ *Return:0 (success)
+ */
+int print_last_digit(int n)
+{
+int last_digit = n % 10;
+
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + 0);
+return (last_digit);
+}
