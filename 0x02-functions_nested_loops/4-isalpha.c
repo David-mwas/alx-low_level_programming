6802d61440eda353e9550586ee595de8779e3452
David-mwas <dmwas704@gmail.com>
2022-09-15 14:50:01 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ *_isalpha:checks for alphabetic lowercase characters
+ *
+ *Return:0 (success)
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
