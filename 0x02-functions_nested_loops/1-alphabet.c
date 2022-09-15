89c7f74334533133788a6cba676ddd9528a15fb7
David-mwas <dmwas704@gmail.com>
2022-09-15 13:46:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main func descrip do something
+ *
+ *Return:0 (success)
+ */
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
