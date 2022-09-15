630230180173aa9320184109bf87a153fdf158df
David-mwas <dmwas704@gmail.com>
2022-09-15 14:24:21 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ *_islower-checks for lowerc characters
+ *
+ *Return:0 (success)
+ */
+int _islower(int c)
+{
+if ((c >= 'a' && c <= 'z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
