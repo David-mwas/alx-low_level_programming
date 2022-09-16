03e4ef26178984a6959b4913bc29b6df11c0c1cf
David-mwas <dmwas704@gmail.com>
2022-09-15 17:27:04 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ *print_to_98-Entry point
+ *description-prints numbers upto 98
+ *parameter-n-integer parameter "int n"
+ *int n-parameter
+ *Return:always 0 success
+ */
 void print_to_98(int n)
 {
 if (n >= 98)
@@ -11,7 +18,7 @@ printf("%d\n", n);
 else
 {
 while (n < 98)
-printf("%d, ",n++);
+printf("%d, ", n++);
 printf("%d\n", n);
 }
 }
