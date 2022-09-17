fed8cbb3489ef857674b58004dc45ac79fa03236
David-mwas <dmwas704@gmail.com>
2022-09-17 15:46:15 -0700

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *largest_number-returns largest number
+ *
+ *@a:1st param
+ *@b:2nd param
+ *@c:3rd param
+ *@largest:largest
+ *Return : 0 (success)
+ */
+int largest_number(int a, int b, int c)
+{
+int largest;
+if (a >= b && a >= c)
+{
+largest = a;
+}
+else if (b >= a && b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+return (largest);
+}
