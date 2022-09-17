c3f3f6dfd44daa390a4bd214dc3102cb9f925105
David-mwas <dmwas704@gmail.com>
2022-09-17 14:35:48 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main-test func for +ve,-ve
+ *@void:no params
+ *Return : 0 (success)
+ */
+int main(void)
+{
+int i;
+i = 0;
+
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*Function that give the sighn of any num*/
+void positive_or_negative(int i);
+
+/*Function that give the largest number of the 3*/
+int largest_number(int a, int b, int c);
+
+/*Function that convert day*/
+int convert_day(int month, int day);
+
+/*Function that print remaining days*/
+void print_remainig_days(int month, int day, int year);
+#endif /*MAIN_H*/
