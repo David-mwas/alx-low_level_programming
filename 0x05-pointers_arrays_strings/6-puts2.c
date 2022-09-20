40b1fa1f5b201e0d116fbccc8be2c836e81dd98d
David-mwas <dmwas704@gmail.com>
2022-09-20 11:39:04 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "unistd.h"
+/**
+ *puts2 - reverse q string
+ *@str: The string to be reversed
+ */
+void puts2(char *str)
+{
+int len = 0, index = 0;
+char tmp;
+while (str[index++])
+{
+len++;
+}
+for (index = 0 ; index < len ; index += 2)
+{
+_putchar(str[index]);
+_putchar(n);
+}
+}
