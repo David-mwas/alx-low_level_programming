7265d0cb7bb8d906877d38b44b3eeda90b63212a
David-mwas <dmwas704@gmail.com>
2022-09-20 12:34:17 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "unistd.h"
+/**
+ *_strcpy - reverse q string
+ *@dest: buffer tocopy string to
+ *@src: the source string tocopy
+ *
+ *Return: a pointer to destination
+ */
+char *_strcpy(char *dest, const char *src)
+{
+int index = 0;
+while (src[index])
+{
+dest[index] = src[index];
+index++;
+}
+return (dest);
+}
