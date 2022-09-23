dfc277750719f04bc3869da6f7ec9d0b4bdef9f1
David-mwas <dmwas704@gmail.com>
2022-09-23 12:44:24 -0700

    fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,4 @@
-
 #include "main.h"
-
 /**
  * string_toupper - Changes all lowercase letters
  *                  of a string to uppercase.
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -7,10 +8,10 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
