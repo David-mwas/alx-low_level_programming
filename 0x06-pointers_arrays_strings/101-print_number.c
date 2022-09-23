5cfa680552a17dbca96317eeb6fa6a59e36050c7
David-mwas <dmwas704@gmail.com>
2022-09-23 13:30:43 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *strcat - reverse q string
+ *@dest: buffer tocopy string to
+ *@src: the source string tocopy
+ *
+ *Return: a pointer to destination
+ */
+char *strcat(char *dest, const char *src)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+*(p + 5) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
+
+}
