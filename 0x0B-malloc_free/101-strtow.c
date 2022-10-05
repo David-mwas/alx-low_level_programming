4c91cb53165bbd2e11c8047af3c12a9e23f9866d
DavidMwas <100795187+David-mwas@users.noreply.github.com>
2022-10-05 21:20:35 +0300

    Last task today woo

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,9 +13,9 @@ int i, n = 0;
 
 for (i = 0; s[i]; i++)
 {
-if (s[i] ==  )
+if (s[i] == ' ')
 {
-if (s[i + 1] !=   && s[i + 1] != 0)
+if (s[i + 1] != ' ' && s[i + 1] != '\0')
 n++;
 }
 else if (i == 0)
@@ -36,7 +36,7 @@ char **strtow(char *str)
 int i, j, k, l, n = 0, wc = 0;
 char **w;
 
-if (str == NULL || *str == 0)
+if (str == NULL || *str == '\0')
 return (NULL);
 n = wrdcnt(str);
 if (n == 1)
@@ -48,9 +48,9 @@ w[n - 1] = NULL;
 i = 0;
 while (str[i])
 {
-if (str[i] !=   && (i == 0 || str[i - 1] ==  ))
+if (str[i] != 'n' && (i == 0 || str[i - 1] == ' '))
 {
-for (j = 1; str[i + j] !=   && str[i + j]; j++)
+for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
 w[wc] = (char *)malloc(j * sizeof(char));
@@ -65,7 +65,7 @@ return (NULL);
 }
 for (l = 0; l < j; l++)
 w[wc][l] = str[i + l];
-w[wc][l] = 0;
+w[wc][l] = '\0';
 wc++;
 i += j;
 }
