97808393fccfc9d8ad1116d93b9c375039695689
DavidMwas <100795187+David-mwas@users.noreply.github.com>
2022-10-05 22:08:59 +0300

    Last paining hot fix 🔥

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,9 +13,9 @@ int i, n = 0;
 
 for (i = 0; s[i]; i++)
 {
-if (s[i] ==  )
+if (s[i] == ' ')
 {
-if (s[i + 1] !=   && s[i + 1] != 0)
+if (s[i + 1] != ' ' && s[i + 1] != '\0')
 n++;
 }
 else if (i == 0)
@@ -41,19 +41,19 @@ return (NULL);
 n = wrdcnt(str);
 if (n == 1)
 return (NULL);
-w = (char **)malloc(n * sizeof(char *));
+w = (char **)malloc(n *sizeof(char *));
 if (w == NULL)
 return (NULL);
 w[n - 1] = NULL;
 i = 0;
 while (str[i])
 {
-if (str[i] !=   && (i == 0 || str[i - 1] ==  ))
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 {
-for (j = 1; str[i + j] !=   && str[i + j]; j++)
+for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
-w[wc] = (char *)malloc(j * sizeof(char));
+w[wc] = (char *)malloc(j *sizeof(char));
 j--;
 if (w[wc] == NULL)
 {
