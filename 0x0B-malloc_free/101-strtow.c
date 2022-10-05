c7c015177947446f4c98d4ae94eefa3bc7907121
David-mwas <dmwas704@gmail.com>
2022-10-05 11:36:01 -0700

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 #include <stdlib.h>
 
@@ -5,6 +6,7 @@
  * wrdcnt - counts the number of words in a string
  * @s: string to count
  *
+
  * Return: int of number of words
  */
 int wrdcnt(char *s)
@@ -13,9 +15,9 @@ int i, n = 0;
 
 for (i = 0; s[i]; i++)
 {
-if (s[i] ==  )
+if (s[i] == ' ')
 {
-if (s[i + 1] !=   && s[i + 1] != 0)
+if (s[i + 1] != ' ' && s[i + 1] != 0)
 n++;
 }
 else if (i == 0)
@@ -48,9 +50,9 @@ w[n - 1] = NULL;
 i = 0;
 while (str[i])
 {
-if (str[i] !=   && (i == 0 || str[i - 1] ==  ))
+if (str[i] != ' ' && (i == 0 || str[i - 1] ==  ))
 {
-for (j = 1; str[i + j] !=   && str[i + j]; j++)
+for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
 w[wc] = (char *)malloc(j * sizeof(char));
