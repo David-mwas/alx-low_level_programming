bd86fa0f7ffa5e591eb1e59aacc4a0ec05d10770
DavidMwas <100795187+David-mwas@users.noreply.github.com>
2022-10-05 21:40:09 +0300

    Last task hot fix wooo

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -41,7 +41,7 @@ return (NULL);
 n = wrdcnt(str);
 if (n == 1)
 return (NULL);
-w = (char **)malloc(n * sizeof(char *));
+w = (char **)malloc(n *sizeof(char *));
 if (w == NULL)
 return (NULL);
 w[n - 1] = NULL;
@@ -53,7 +53,7 @@ if (str[i] != 'n' && (i == 0 || str[i - 1] == ' '))
 for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
-w[wc] = (char *)malloc(j * sizeof(char));
+w[wc] = (char *)malloc(j *sizeof(char));
 j--;
 if (w[wc] == NULL)
 {
