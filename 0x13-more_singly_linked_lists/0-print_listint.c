8a3e18499ea2efbf79362a0a850dbfab712f84f1
DavidMwas <100795187+David-mwas@users.noreply.github.com>
2022-10-27 23:08:27 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,34 +1,18 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
-typedef struct listint_s
+  * print_listint - prints all elements of a list_t list
+  * @h: singly linked list to print
+  * Return: number of nodes in the list
+  */
+size_t print_listint(const listint_t *h)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
-/**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the list_t list.
- *
- * Return: The number of nodes in the list_t list.
- */
- size_t print_listint(const listint_t *h)
-{
-	size_t nodes = 0;
+	size_t i;
 
-	while (h)
+	for (i = 0; h; i++)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-
-	return (nodes);
+	return (i);
 }
